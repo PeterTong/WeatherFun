d03bbd270028ff685f3c92d0d5ffae92718e650d
PeterTong <tkwpeter21@gmail.com>
2016-07-01 22:34:30 +0800

    Initial Commit

---
+++ WeatherFun/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherFun
+//
+//  Created by KwokWing Tong on 1/7/2016.
+//  Copyright © 2016 Tong Kwok Wing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
