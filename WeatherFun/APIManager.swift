5f032b7e38356217d9514fb462d0f567a5075259
PeterTong <tkwpeter21@gmail.com>
2016-07-02 19:15:35 +0800

    Completed APIManager for convert json to Object(string , int)  to show current weather condition/ situation. In ViewController, implement loadData() in APIManager

--- WeatherFun/APIManager.swift
+++ WeatherFun/APIManager.swift
@@ -10,6 +10,54 @@ import Foundation
 
 class APIManager {
 	
+	func loadData(urlString: String, completion: [Weather] -> Void){
+		
+		let config = NSURLSessionConfiguration.ephemeralSessionConfiguration()
+		
+		let session = NSURLSession(configuration: config)
+		
+		let url = NSURL(string: urlString)!
+		
+		let task = session.dataTaskWithURL(url) { (data, response, error) in
+			
+			if error != nil {
+				print(error!.localizedDescription)
+			}else{
+				//Added for JSONSerialization
+				do{
+					/* .AllowFragments - top level object is not Array or Dictionary.
+					Any type of string or value
+					NSJSONSerialization requires the Do / Try / Catch
+					Converts the NSDATA into a JSON Object and cast it to a Dictionary */
+					
+					if let json = try NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments) as? JSONDictionary, currently = json["currently"] as? JSONDictionary {
+						
+						var weathers = [Weather]()
+						let curweather = Weather(currentWeatherData: currently )
+						weathers.append(curweather)
+						
+						let i = weathers.count
+						print("Weather - total count --> \(i)")
+						print(" ")
+						
+						let priority = DISPATCH_QUEUE_PRIORITY_DEFAULT
+						dispatch_async(dispatch_get_global_queue(priority, 0)){
+							dispatch_async(dispatch_get_main_queue()){
+								completion(weathers)
+							}
+						}
+						
+					}
+				}catch{
+					print("error in NSJSONSerializtion")
+				}
+				
+			}
+			
+		}
+		task.resume()
+	}
+	
 	
 	
 }
--- WeatherFun/ViewController.swift
+++ WeatherFun/ViewController.swift
@@ -9,16 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+	
+	var curweathers:[Weather]!
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+		
+		
+		let api = APIManager()
+		api.loadData("https://api.forecast.io/forecast/04f92db01ba574feb675c1e7642619b2/22.339337,114.109066?units=si",completion:didLoadData)
+	}
+	
+	func didLoadData(weathers:[Weather]){
+		
+		curweathers = weathers
+		
+		for temperature in weathers{
+			print("Current Temperature: \(temperature.currentTemperature)")
+		}
+		
+		for summary in weathers{
+			print("Summary: \(summary.currentSummary)")
+		}
 	}
 
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
-	}
+	
+	
+	
 
 
 }
